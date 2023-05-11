65aafac3cf1fd0b8db5747a4abe6d0bb0b2ab3d5
dariogoetz <8102034+dariogoetz@users.noreply.github.com>
2023-05-11 19:06:22 +0000

    Deploying to gh-pages from @ dariogoetz/mpw-webapp@a5f8fc28074a34fbc67e6086f3fb8179dee6f3a5 🚀

--- mpw-leptos-cb48397de9149f79.js
+++ mpw-leptos-cb48397de9149f79.js
@@ -594,7 +594,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('mpw-leptos-f916690a61a38c67_bg.wasm', import.meta.url);
+        input = new URL('mpw-leptos-cb48397de9149f79_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
